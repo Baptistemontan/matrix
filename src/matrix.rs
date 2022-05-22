37e40f207af32c4fba05ba7274fd1ad416a8fd62
Baptiste Montangon de <baptiste.de.montangon@hva.nl>
2022-05-22 20:32:01 +0200

    start of matrix impl

---
+++ src/matrix.rs
@@ -0,0 +1,37 @@
+use std::ops::{Deref, DerefMut};
+
+use crate::vector::RowVector;
+
+
+
+pub struct Matrix(Vec<RowVector>);
+
+impl Deref for Matrix {
+    type Target = [RowVector];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Matrix {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::vector::Vector;
+
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let mut mat = Matrix(vec![RowVector::new(2); 4]);
+
+        mat[0][1] = 4.0;
+
+        assert_eq!(mat[0][1], 4.0);
+    }
+}
